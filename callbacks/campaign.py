490e098f6d62ed7075648e8d6a456c004edd7520
Sea Yi Theng <yitheng.sea@petronas.com>
2021-06-15 21:45:57 +0800

    added campaign tabs for callbacks n views

---
+++ callbacks/campaign.py
@@ -0,0 +1,225 @@
+import json
+import requests
+import pandas as pd
+from datetime import datetime
+from dash.dependencies import Input, Output, State
+import dash_table
+
+
+from app import app
+from src.viz import *
+from conf import loguru_logger as _logger
+
+API_URL = "http://localhost:3000/run_optimisation/"
+
+
+# ====================================================================================================
+# Tactical Model Data Table Column Update
+# ==================================================================================================== 
+
+@app.callback(
+    [Output('campaign-tab1-product-data-cost-datatable', 'columns'),
+    Output('campaign-tab1-product-data-profit-datatable', 'columns'),
+    Output('campaign-tab1-kpi-budget-div', 'children'),
+    Output('campaign-tab1-kpi-roi-div', 'children')],
+    [Input('campaign-tab1-cluster-data-datatable', 'data'),
+    Input('budget-value', 'value'), Input('min-roi-value', 'value')],
+    prevent_initial_call=True)
+
+def update_datatable(cluster_data, budget, roi):
+    # removing row with empty cells
+    def filter_empty_cell(data):
+        clean_data = []
+        for row in data:
+            # require that all elements in a row are specified
+            if all([cell != '' for cell in row.values()]):
+                clean_data.append(row)
+        return clean_data
+
+    cluster_data_clean = filter_empty_cell(cluster_data)
+    clus_prod_cost_columns = [{'id': 'Product_Type_Cost', 'name': 'ProductType/Cost(RM)'}]
+    clus_prod_profit_columns = [{'id': 'Product_Type_Profit', 'name': 'ProductType/Profit(RM)'}]
+    for list_dic in cluster_data_clean:
+        clus_prod_cost_columns.append({"id": list_dic['Cluster'], "name": list_dic['Cluster']})
+        clus_prod_profit_columns.append({"id": list_dic['Cluster'], "name": list_dic['Cluster']})
+    roi = '{:,.1f} %'.format(roi)
+    budget = 'RM {:,.2f}'.format(budget)
+    return [clus_prod_cost_columns, clus_prod_profit_columns, budget, roi]
+
+# ====================================================================================================
+# Tactical Model Output: Campaign Cluster Product Assignment Tables
+# ====================================================================================================
+@app.callback(
+    [Output('campaign-tab1-opt-roi-div', 'children'),
+    Output('campaign-tab1-opt-profit-div', 'children'),
+    Output('campaign-tab1-opt-cost-div', 'children'),
+    Output('campaign-tab1-results-datatable', 'children')],
+    [Input('campaign-tab1-btn-run-optimisation', 'n_clicks')],
+    [State('budget-value', 'value'), State('min-roi-value', 'value'),
+    State('campaign-tab1-cluster-data-datatable', 'data'),
+    State('campaign-tab1-product-data-datatable', 'data'),
+    State('campaign-tab1-product-data-cost-datatable', 'data'),
+    State('campaign-tab1-product-data-profit-datatable', 'data'),
+    State('campaign-tab2-customer-data-datatable', 'data')],
+    prevent_initial_call=True
+)
+
+# callback to display tactical model output
+def cluster_marketing_allocation(
+    n_click, budget, ROI, cluster_data, product_data, product_cost_data, product_profit_data, customer_data_costprofit):
+
+    _logger.info(f"[customer_marketing_allocation] Tactical Model Initiated.")
+
+    # removing row with empty cells
+    def filter_empty_cell(data):
+        clean_data = []
+        for row in data:
+            # require that all elements in a row are specified
+            if all([cell != '' for cell in row.values()]):
+                clean_data.append(row)
+        return clean_data
+
+    # Consolidating inputs into JSON for post request
+    api_json = {
+        "budget": budget,
+        "roi": ROI,
+        "cluster": filter_empty_cell(cluster_data),
+        "product": filter_empty_cell(product_data),
+        "cost": filter_empty_cell(product_cost_data),
+        "profit": filter_empty_cell(product_profit_data),
+        "cust_cost_profit": filter_empty_cell(customer_data_costprofit)
+    }
+    api_json = json.dumps(api_json)  # Converting dict to json, especially to cater for True/False json types
+    _logger.debug(f"[customer_marketing_allocation] Tactical Model api_json --> {api_json}")
+
+    # Making POST request to API
+    _logger.debug(f"[customer_marketing_allocation] Making API call to {API_URL}")
+    api_response = requests.post(API_URL, data=api_json).json()
+    #_logger.debug(f"[customer_marketing_allocation] api_response --> {api_response}")
+    
+    # Converting results json to dataframes for visualisation
+    _logger.debug(f"[customer_marketing_allocation] Converting JSON responses to pd.DataFrame")
+    api_response = dict(api_response)
+
+    # Converting KPIs for visualisation
+    _logger.debug(f"[customer_marketing_allocation] Converting KPI values to dash column")
+    tactical_expected_money = pd.DataFrame(api_response['tactical_expected_money'])
+    optimal_roi = '{:,.1f} %'.format(tactical_expected_money['optimal_ROI'][0])
+    optimal_profit = 'RM {:,.2f}'.format(tactical_expected_money['money_expected_profit'][0].astype(float))
+    optimal_cost = 'RM {:,.2f}'.format(tactical_expected_money['money_expected_cost'][0].astype(float))
+    #cost_budget_diff = '+/- {:,.2f}'.format(tactical_expected_money['increased_budget'][0].astype(float))
+
+    # Converting cluster product assignment results dash table for visualisation
+    _logger.debug(f"[customer_marketing_allocation] Converting Cluster Product Assignment JSON to datatable")
+    clus_prod_data = api_response['cluster_product_assignment_data']
+    cluster_product_assignment_data = pd.DataFrame(api_response['cluster_product_assignment_data']).reset_index(drop=True)
+    clus_prod_columns = [{"name": i, "id": i, } for i in (cluster_product_assignment_data.columns)]
+    table_output = dash_table.DataTable(
+                        data=clus_prod_data, columns=clus_prod_columns,
+                        style_as_list_view=True,
+                        style_cell={'padding': '5px',
+                                'textAlign': 'center',
+                                'font-family': 'arial'},
+                        style_header={
+                            'backgroundColor': '#20c997',
+                            'fontWeight': 'bold'},)
+    return [optimal_roi, optimal_profit, optimal_cost, table_output]
+
+# ====================================================================================================
+# Operational Model: Campaign Cluster Customer Product Assignment Tables
+# ====================================================================================================
+@app.callback(
+    [Output('campaign-tab2-kpi-budget-div', 'children'),
+    Output('campaign-tab2-kpi-roi-div', 'children'),
+    Output('campaign-tab2-kpi-profit-div', 'children'),
+    Output('campaign-tab2-kpi-cost-div', 'children'),
+    Output('campaign-tab2-results-datatable', 'children')],
+    [Input('campaign-tab2-btn-run-optimisation', 'n_clicks')],
+    [State('budget-value', 'value'), State('min-roi-value', 'value'),
+    State('campaign-tab1-cluster-data-datatable', 'data'),
+    State('campaign-tab1-product-data-datatable', 'data'),
+    State('campaign-tab1-product-data-cost-datatable', 'data'),
+    State('campaign-tab1-product-data-profit-datatable', 'data'),
+    State('campaign-tab2-customer-data-datatable', 'data')],
+    prevent_initial_call=True
+)
+
+def customer_marketing_allocation(
+    n_clicks, budget, ROI, cluster_data, product_data, product_cost_data, product_profit_data, customer_data_costprofit):
+
+    start_time = datetime.now()
+    _logger.info(f"[customer_marketing_allocation] Operation Model Initiated.")
+
+    # removing row with empty cells
+    def filter_empty_cell(data):
+        clean_data = []
+        for row in data:
+            # require that all elements in a row are specified
+            if all([cell != '' for cell in row.values()]):
+                clean_data.append(row)
+        return clean_data
+
+    # Consolidating inputs into JSON for post request
+    api_json = {
+        "budget": budget,
+        "roi": ROI,
+        "cluster": filter_empty_cell(cluster_data),
+        "product": filter_empty_cell(product_data),
+        "cost": filter_empty_cell(product_cost_data),
+        "profit": filter_empty_cell(product_profit_data),
+        "cust_cost_profit": filter_empty_cell(customer_data_costprofit)
+    }
+    api_json = json.dumps(api_json)  # Converting dict to json, especially to cater for True/False json types
+    _logger.debug(f"[customer_marketing_allocation] Operational Model api_json --> {api_json}")
+
+    # Making POST request to API
+    _logger.debug(f"[customer_marketing_allocation] Making API call to {API_URL}")
+    api_response = requests.post(API_URL, data=api_json).json()
+    #_logger.debug(f"[customer_marketing_allocation] api_response --> {api_response}")
+    
+    # Converting results json to dataframes for visualisation
+    _logger.debug(f"[customer_marketing_allocation] Converting JSON responses to pd.DataFrame")
+    api_response = dict(api_response)
+
+    operational_expected_money = pd.DataFrame(api_response['operational_expected_money'])
+    revised_budget = 'RM {:,.2f}'.format(pd.DataFrame(api_response['tactical_expected_money'])['money_expected_cost'][0].astype(float))
+    optimal_roi = '{:,.1f} %'.format(operational_expected_money['customer_optimal_ROI'][0])
+    optimal_profit = 'RM {:,.2f}'.format(operational_expected_money['customer_expected_profit'][0].astype(float))
+    optimal_cost = 'RM {:,.2f}'.format(operational_expected_money['customer_expected_cost'][0].astype(float))
+
+    # Converting cluster product customer assignment results dash table for visualisation
+    _logger.debug(f"[customer_marketing_allocation] Converting Cluster Product Customer Assignment JSON to datatable")
+    clus_prod_cust_data = api_response['cluster_product_customer_assignment_data']
+    cluster_product_cust_assignment_data = pd.DataFrame(api_response['cluster_product_customer_assignment_data']).reset_index(drop=True)
+    clus_prod_columns = [{"name": i, "id": i, } for i in (cluster_product_cust_assignment_data.columns)]
+    table_output = dash_table.DataTable(
+                        data=clus_prod_cust_data, columns=clus_prod_columns,
+                        style_as_list_view=True,
+                        style_cell={'padding': '5px',
+                                'textAlign': 'center',
+                                'font-family': 'arial'},
+                        style_header={
+                            'backgroundColor': '#20c997',
+                            'fontWeight': 'bold'},
+                        style_data_conditional=[
+                            {
+                                'if': {
+                                    'filter_query': '{Selected} = 1',
+                                },
+                                'backgroundColor': '#f0ad4e',
+                                'color': 'white',
+                                'fontWeight': 'bold'
+                            }, 
+                            {
+                                'if': {
+                                    'column_id': 'Selected',},
+                                    'display': 'None',
+                            },
+                        ],
+                        style_header_conditional=[
+                            {
+                                'if': {
+                                    'column_id': 'Selected',},
+                                    'display': 'None',}
+                        ],)
+    return [revised_budget, optimal_roi, optimal_profit, optimal_cost, table_output]
--- views/campaign.py
+++ views/campaign.py
@@ -1,12 +1,12 @@
 import dash_table
 import dash_html_components as html
 import dash_bootstrap_components as dbc
-
+import dash_core_components as dcc
 
 # ================================================================================
-# Tab 1: Strategic Campaign Planning
+# Tab 1: Tactical Campaign Planning
 # ================================================================================
-tab1_strategic_content = dbc.Card([
+tab1_tactical_content = dbc.Card([
 
     # ============================== Top Section: Settings ==============================
     html.H4("Campaign Inputs"),
@@ -24,7 +24,7 @@ tab1_strategic_content = dbc.Card([
                     data=[
                              {'Cluster': 'k1', 'Count': 5},
                              {'Cluster': 'k2', 'Count': 5}
-                         ] + [{'Cluster': '', 'Count': ''} for i in range(5)],
+                         ] + [{'Cluster': '', 'Count': ''} for i in range(2)],
                     # Create additional blank rows for user input
                     editable=True,
                     style_as_list_view=True,
@@ -39,21 +39,75 @@ tab1_strategic_content = dbc.Card([
             )
         ]),
         dbc.Col([
-            html.H6("Product Data"),
+            html.H6("Product Data: Count"),
             html.Div(
                 dash_table.DataTable(
                     id='campaign-tab1-product-data-datatable',
                     columns=(
                         [{'id': 'Product_Type', 'name': 'Product Type'},
-                         {'id': 'Count', 'name': 'Count'},
-                         {'id': 'Profit_Per_Unit', 'name': 'Profit Per Unit (RM)'},
-                         {'id': 'Cost_Per_Unit', 'name': 'Cost Per Unit (RM)'}]
+                         {'id': 'Count', 'name': 'Count'}]
                     ),
                     data=[
-                             {'Product_Type': 'P1', 'Count': 2, 'Profit_Per_Unit': 60, 'Cost_Per_Unit': 15},
-                             {'Product_Type': 'P2', 'Count': 2, 'Profit_Per_Unit': 40, 'Cost_Per_Unit': 5},
-                         ] + [{'Product_Type': '', 'Count': '', 'Profit_Per_Unit': '', 'Cost_Per_Unit': ''}
-                              for i in range(5)],
+                             {'Product_Type': 'p1', 'Count': 2},
+                             {'Product_Type': 'p2', 'Count': 2},
+                         ] + [{'Product_Type': '', 'Count': ''}
+                             for i in range(2)],
+                    # Create additional blank rows for user input
+                    editable=True,
+                    style_as_list_view=True,
+                    style_cell={'padding': '5px',
+                                'textAlign': 'center',
+                                'font-family': 'arial'},
+                    style_header={
+                        'backgroundColor': '#ADD8E6',
+                        'fontWeight': 'bold'
+                    },
+                ), className='p-4'
+            )
+        ]),
+        dbc.Col([
+            html.H6("Product Data: Cost"),
+            html.Div(
+                dash_table.DataTable(
+                    id='campaign-tab1-product-data-cost-datatable',
+                    columns=(
+                        [{'id': 'Product_Type_Cost', 'name': 'ProductType/Cost(RM)'},
+                         {'id': 'k1', 'name': 'k1'},
+                         {'id': 'k2', 'name': 'k2'}]
+                    ),
+                    data=[
+                             {'Product_Type_Cost': 'p1', 'k1': 200, 'k2': 300},
+                             {'Product_Type_Cost': 'p2', 'k1': 100, 'k2': 200},
+                         ] + [{'Product_Type_Cost': '', 'k1': '', 'k2': ''}
+                             for i in range(2)],
+                    # Create additional blank rows for user input
+                    editable=True,
+                    style_as_list_view=True,
+                    style_cell={'padding': '5px',
+                                'textAlign': 'center',
+                                'font-family': 'arial'},
+                    style_header={
+                        'backgroundColor': '#ADD8E6',
+                        'fontWeight': 'bold'
+                    },
+                ), className='p-4'
+            )
+        ]),
+        dbc.Col([
+            html.H6("Product Data: Profit"),
+            html.Div(
+                dash_table.DataTable(
+                    id='campaign-tab1-product-data-profit-datatable',
+                    columns=(
+                        [{'id': 'Product_Type_Profit', 'name': 'ProductType/Profit(RM)'},
+                         {'id': 'k1', 'name': 'k1'},
+                         {'id': 'k2', 'name': 'k2'}]
+                    ),
+                    data=[
+                             {'Product_Type_Profit': 'p1', 'k1': 2000, 'k2': 3000},
+                             {'Product_Type_Profit': 'p2', 'k1': 1000, 'k2': 2000},
+                         ] + [{'Product_Type_Profit': '', 'k1': '', 'k2': ''}
+                              for i in range(2)],
                     # Create additional blank rows for user input
                     editable=True,
                     style_as_list_view=True,
@@ -69,8 +123,32 @@ tab1_strategic_content = dbc.Card([
         ]),
     ]),
 
-    dbc.Button("Run Optimisation", id='campaign-tab1-btn-run-optimisation', className='campaign-btn m-4'),
+    html.Hr(),
+    dbc.Row([
+        dbc.Col([
+            html.H6("Campaign Budget"),
+            dbc.InputGroup([
+                dbc.InputGroupAddon(
+                    "RM", addon_type='prepend'),
+                dcc.Input(
+                    id='budget-value',
+                    type='number',
+                    value=200),
+            ],className='mb-3', size='sm')], width=3),
+        dbc.Col([
+            html.H6("Campaign Minimum ROI"),
+            dbc.InputGroup([
+                dcc.Input(
+                    id='min-roi-value',
+                    type='number',
+                    value=120),
+                dbc.InputGroupAddon(
+                    "%", addon_type='append'),
+            ],className='mb-3', size='sm')], width=3),
+    ]),
+    dbc.Button("Run Optimisation", id='campaign-tab1-btn-run-optimisation', className='campaign-btn m-1'),
     html.Br(),
+
 
     # ============================== Middle Section: KPIs ==============================
     html.H4("Campaign KPIs"),
@@ -78,17 +156,30 @@ tab1_strategic_content = dbc.Card([
     dbc.Row([
         dbc.Col(width=1),
         dbc.Col(dbc.Card([
-            html.H6("ROI"),
-            html.Div("99.9%", id='campaign-tab1-kpi-roi-div')  # You can put icons in this div too
-        ], body=True, className='campaign-card')),
+            html.I(className='fas fa-funnel-dollar'),
+            html.H6("Initial Budget"), #dbc.CardImg(src='/assets/cash-coin.svg', style={'height':'20%', 'width':'20%'}),
+            html.Div("RM 200.00", id='campaign-tab1-kpi-budget-div'),# You can put icons in this div too
+        ], body=True, className='campaign-card', color="success", inverse=True)),
         dbc.Col(dbc.Card([
+            html.I(className='fas fa-percentage'),
+            html.H6("Minimum ROI"),
+            html.Div("120.0 %", id='campaign-tab1-kpi-roi-div')  # You can put icons in this div too
+        ], body=True, className='campaign-card', color="warning", inverse=True)),
+        dbc.Col(dbc.Card([
+            html.I(className='fas fa-percentage'),
+            html.H6("Optimal ROI"),
+            html.Div("? %", id='campaign-tab1-opt-roi-div')  # You can put icons in this div too
+        ], body=True, className='campaign-card', color="info", inverse=True)),
+        dbc.Col(dbc.Card([
+            html.I(className='fas fa-users'),
             html.H6("Profit"),
-            html.Div("RM 100,000", id='campaign-tab1-kpi-profit-div')  # You can put icons in this div too
-        ], body=True, className='campaign-card')),
+            html.Div("RM ?", id='campaign-tab1-opt-profit-div')# You can put icons in this div too
+        ], body=True, className='campaign-card', color="#6f42c1", inverse=True)),
         dbc.Col(dbc.Card([
+            html.I(className='fas fa-dollar-sign'),
             html.H6("Cost"),
-            html.Div("RM 10,000", id='campaign-tab1-kpi-cost-div')  # You can put icons in this div too
-        ], body=True, className='campaign-card')),
+            html.Div("RM ?", id='campaign-tab1-opt-cost-div'),  # You can put icons in this div too
+        ], body=True, className='campaign-card', color="danger", inverse=True)),
         dbc.Col(width=1),
     ]),
 
@@ -96,41 +187,55 @@ tab1_strategic_content = dbc.Card([
     # ============================== Bottom Section: Results ==============================
     html.H4("Optimisation Results"),
     html.Hr(),
-    dbc.Row([
-        dash_table.DataTable(id='campaign-tab1-results-datatable')
-    ]),
-
-    html.Br(),
-
-
+    dbc.Col([
+        html.Div(id='campaign-tab1-results-datatable')]),
+        
 ], body=True)
 
 
 # ================================================================================
-# Tab 2: Tactical Campaign Planning
+# Tab 2: Operational Campaign Planning
 # ================================================================================
-tab2_tactical_content = dbc.Card([
+tab2_operational_content = dbc.Card([
 
     # ============================== Top Section: Settings ==============================
     html.H4("Campaign Inputs"),
     html.Hr(),
-    dbc.Row([
+    dbc.Col([
         dbc.Col([
             html.H6("Customer Data"),
             html.Div(
                 dash_table.DataTable(
                     id='campaign-tab2-customer-data-datatable',
                     columns=(
-                        [{'id': 'Customer', 'name': 'Customer'},
+                        [{'id': 'Cluster', 'name': 'Cluster'},
+                         {'id': 'Customer', 'name': 'Customer'},
                          {'id': 'Product', 'name': 'Product'},
                          {'id': 'Cost', 'name': 'Cost (RM)'},
                          {'id': 'Profit', 'name': 'Profit (RM)'}]
                     ),
                     data=[
-                             {'Customer': 'C1', 'Product': 'P1', 'Cost': 20, 'Profit': 300},
-                             {'Customer': 'C2', 'Product': 'P1', 'Cost': 30, 'Profit': 400},
-                             {'Customer': 'C3', 'Product': 'P2', 'Cost': 25, 'Profit': 550},
-                         ] + [{'Cluster': '', 'Count': ''} for i in range(2)],
+                             {'Cluster': 'k1', 'Customer': 'c1', 'Product': 'p1', 'Cost': 205, 'Profit': 2050},
+                             {'Cluster': 'k1', 'Customer': 'c2', 'Product': 'p1', 'Cost': 195, 'Profit': 1950},
+                             {'Cluster': 'k1', 'Customer': 'c3', 'Product': 'p1', 'Cost': 200, 'Profit': 2000},
+                             {'Cluster': 'k1', 'Customer': 'c4', 'Product': 'p1', 'Cost': 210, 'Profit': 2100},
+                             {'Cluster': 'k1', 'Customer': 'c5', 'Product': 'p1', 'Cost': 190, 'Profit': 1900},
+                             {'Cluster': 'k2', 'Customer': 'c6', 'Product': 'p1', 'Cost': 300, 'Profit': 3000},
+                             {'Cluster': 'k2', 'Customer': 'c7', 'Product': 'p1', 'Cost': 290, 'Profit': 2900},
+                             {'Cluster': 'k2', 'Customer': 'c8', 'Product': 'p1', 'Cost': 305, 'Profit': 3050},
+                             {'Cluster': 'k2', 'Customer': 'c9', 'Product': 'p1', 'Cost': 310, 'Profit': 3100},
+                             {'Cluster': 'k2', 'Customer': 'c10', 'Product': 'p1', 'Cost': 295, 'Profit': 2950},
+                             {'Cluster': 'k1', 'Customer': 'c1', 'Product': 'p2', 'Cost': 105, 'Profit': 1050},
+                             {'Cluster': 'k1', 'Customer': 'c2', 'Product': 'p2', 'Cost': 95, 'Profit': 950},
+                             {'Cluster': 'k1', 'Customer': 'c3', 'Product': 'p2', 'Cost': 100, 'Profit': 1000},
+                             {'Cluster': 'k1', 'Customer': 'c4', 'Product': 'p2', 'Cost': 110, 'Profit': 1100},
+                             {'Cluster': 'k1', 'Customer': 'c5', 'Product': 'p2', 'Cost': 90, 'Profit': 900},
+                             {'Cluster': 'k2', 'Customer': 'c6', 'Product': 'p2', 'Cost': 200, 'Profit': 2000},
+                             {'Cluster': 'k2', 'Customer': 'c7', 'Product': 'p2', 'Cost': 190, 'Profit': 1900},
+                             {'Cluster': 'k2', 'Customer': 'c8', 'Product': 'p2', 'Cost': 205, 'Profit': 2050},
+                             {'Cluster': 'k2', 'Customer': 'c9', 'Product': 'p2', 'Cost': 210, 'Profit': 2100},
+                             {'Cluster': 'k2', 'Customer': 'c10', 'Product': 'p2', 'Cost': 195, 'Profit': 1950},
+                         ]  + [{'Cluster': '', 'Customer': '', 'Product': '', 'Cost': '', 'Profit': ''} for i in range(10)],
                     # Create additional blank rows for user input
                     editable=True,
                     style_as_list_view=True,
@@ -150,23 +255,32 @@ tab2_tactical_content = dbc.Card([
     dbc.Button("Run Optimisation", id='campaign-tab2-btn-run-optimisation', className='campaign-btn m-4'),
     html.Br(),
 
+
     # ============================== Middle Section: KPIs ==============================
     html.H4("Campaign KPIs"),
     html.Hr(),
     dbc.Row([
         dbc.Col(width=1),
         dbc.Col(dbc.Card([
-            html.H6("ROI"),
-            html.Div("99.9%", id='campaign-tab2-kpi-roi-div')  # You can put icons in this div too
-        ], body=True, className='campaign-card')),
+            html.I(className='fas fa-funnel-dollar'),
+            html.H6("Revised Budget"),
+            html.Div("RM ?", id='campaign-tab2-kpi-budget-div')  # You can put icons in this div too
+        ], body=True, className='campaign-card', color="success", inverse=True)),
         dbc.Col(dbc.Card([
+            html.I(className='fas fa-percentage'),
+            html.H6("Optimal ROI"),
+            html.Div("? %", id='campaign-tab2-kpi-roi-div')  # You can put icons in this div too
+        ], body=True, className='campaign-card', color="info", inverse=True)),
+        dbc.Col(dbc.Card([
+            html.I(className='fas fa-users'),
             html.H6("Profit"),
-            html.Div("RM 100,000", id='campaign-tab2-kpi-profit-div')  # You can put icons in this div too
-        ], body=True, className='campaign-card')),
+            html.Div("RM ?", id='campaign-tab2-kpi-profit-div')  # You can put icons in this div too
+        ], body=True, className='campaign-card', color="#6f42c1", inverse=True)),
         dbc.Col(dbc.Card([
+            html.I(className='fas fa-dollar-sign'),
             html.H6("Cost"),
-            html.Div("RM 10,000", id='campaign-tab2-kpi-cost-div')  # You can put icons in this div too
-        ], body=True, className='campaign-card')),
+            html.Div("RM ?", id='campaign-tab2-kpi-cost-div')  # You can put icons in this div too
+        ], body=True, className='campaign-card', color="danger", inverse=True)),
         dbc.Col(width=1),
     ]),
 
@@ -174,11 +288,8 @@ tab2_tactical_content = dbc.Card([
     # ============================== Bottom Section: Results ==============================
     html.H4("Optimisation Results"),
     html.Hr(),
-    dbc.Row([
-        dash_table.DataTable(id='campaign-tab2-results-datatable')
-    ]),
-
-    html.Br(),
+    dbc.Col([
+        html.Div(id='campaign-tab2-results-datatable')]),
 
 ], body=True)
 
@@ -193,8 +304,8 @@ layout = dbc.Container([
     html.Br(),
 
     dbc.Tabs([
-        dbc.Tab(tab1_strategic_content, label='Strategic Campaign Planning'),
-        dbc.Tab(tab2_tactical_content, label='Tactical Campaign Planning'),
+        dbc.Tab(tab1_tactical_content, label='Tactical Campaign Planning'),
+        dbc.Tab(tab2_operational_content, label='Operational Campaign Planning'),
     ])
 
 ], className="mt-4")

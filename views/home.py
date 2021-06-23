56c7749a29328df3288b0f03f447198c64f99de1
Winson Yeap <yeap.xinwei@petronas.com>
2021-06-23 19:05:04 +0800

    Added homepage description for Warehouse

--- views/home.py
+++ views/home.py
@@ -1,8 +1,10 @@
+import sd_material_ui as sdmui
 import dash_core_components as dcc
+import dash_html_components as html
 from dash_html_components.Ol import Ol
 import dash_bootstrap_components as dbc
-import dash_html_components as html
-import sd_material_ui as sdmui
+
+from app import app
 
 
 ###############################################################################
@@ -17,7 +19,65 @@ layout = dbc.Container([
     # ============================== Top Section: Warehouse ==============================
     dbc.Card([
         dbc.CardBody([
-            html.Div('Use Case 1: Where to build warehouses ?', style={'color': '#17a2b8', 'fontSize': 30, 'font-weight': 'bold', 'text-align': 'center'}),
+            html.Div('Use Case 1: Where should I place my warehouses ?', 
+                     style={'color': '#17a2b8', 'fontSize': 30, 'font-weight': 'bold', 'text-align': 'center'}),
+            html.Br(),
+            dbc.Row([
+                html.Div(
+                    """
+                    Due to the recent COVID-19 pandemic, our convenience store revenue has fallen due to restricted 
+                    travelling by Malaysian citizens. People are now travelling on the streets less as compared to 
+                    before.
+                    """, className='mt-2'
+                ),
+                html.Br(),
+                html.Div(
+                    """
+                    However, other businesses have gained from the changes brought upon by the pandemic. For example,
+                    online shopping and delivery have become part of the new normal for many households in Malaysia.
+                    """, className='mt-2'
+                ),
+                html.Br(),
+                html.Div(
+                    """
+                    As a move to adapt to the new trend and to promote convenience store sales, PETRONAS has 
+                    launched a new delivery service via our Setel app, where users can make purchases from nearby
+                    Mesra stores and have them delivered to their doorstep.
+                    """, className='mt-2'
+                ),
+                html.Br(),
+                html.Div(
+                    """
+                    This however, requires upfront planning in terms of both warehouses (where to set them up and how
+                    big do they need to be) and manpower (how many despatchers are required per station).
+                    """, className='mt-2'
+                ),
+                html.Br(), html.Br(),
+                html.Img(src=app.get_asset_url('Klang Valley.png'), className='center', style={'width': '50%'}),
+                html.H5(
+                    """
+                    Today, we will explore how we can identify optimum warehouse locations and the number of 
+                    despatchers to cater for the demand around Klang Valley.
+
+                    The model inputs required are:
+                    """, className='mt-4'
+                ),
+            ]),
+            html.Ul(className='Lists'),
+            html.Ol(),
+            html.Li(className='fas fa-check-square fa-lg', style={'color': '#d9534f'}),
+            html.I('  Candidate warehouse locations to be rented', style={'color': '#343a40', 'fontSize': 20, 'text-align': 'center'}),
+            html.Br(),
+            html.Li(className='fas fa-check-square fa-lg', style={'color': '#d9534f'}),
+            html.I('  Despatcher manpower costs', style={'color': '#343a40', 'fontSize': 20, 'text-align': 'center'}),
+            html.Br(),
+            html.Li(className='fas fa-check-square fa-lg', style={'color': '#d9534f'}),
+            html.I('  Delivery costs', style={'color': '#343a40', 'fontSize': 20, 'text-align': 'center'}),
+            html.Br(),
+            html.Li(className='fas fa-check-square fa-lg', style={'color': '#d9534f'}),
+            html.I('  Other delivery constraints and settings (e.g., delivery speed, despatch volume limit, working hours)', 
+                    style={'color': '#343a40', 'fontSize': 20, 'text-align': 'center'}),
+            html.Ul(className='Lists'),
         ]),
     ]),
 
